e6dead40a6db564114ee5ea94389a81bf62f0f01
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2024-02-24 01:56:10 +0530

    Add files via upload

---
+++ functions3.rs
@@ -0,0 +1,15 @@
+// functions3.rs
+//
+// Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
+// hint.
+
+fn main() {
+    let x:u32 = 5;
+    call_me(x);
+}
+
+fn call_me(num: u32) {
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
+    }
+}
