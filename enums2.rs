df90a7dd82cf0fd7cbd390c4d5bac0da38d2e87c
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2024-03-10 21:51:23 +0530

    Create enums2.rs

---
+++ enums2.rs
@@ -0,0 +1,34 @@
+// enums2.rs
+//
+// Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+#[derive(Debug)]
+enum Message {
+    // TODO: define the different variants used below
+    Move { x: u8, y: u8 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
